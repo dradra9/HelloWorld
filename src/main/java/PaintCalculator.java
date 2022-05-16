8ec4f2ccc7621cd71c34add4030a338f4971b341
Melvin <melvinjoy10@gmail.com>
2022-05-16 11:50:47 +0100

    function paint area

--- src/main/java/PaintCalculator.java
+++ src/main/java/PaintCalculator.java
@@ -1,6 +1,29 @@
 import java.util.Scanner;
 
 public class PaintCalculator {
+
+    static double totalWallArea(int numberOfWalls){
+
+        Scanner myScanner = new Scanner(System.in);
+        double totalWallArea = 0;
+
+        for( int i = 1;  i <= numberOfWalls; i++ )
+        {
+            System.out.println("For wall number:  " + numberOfWalls + ". What are the dimensions?" );
+            System.out.println("What is the height of the wall?" );
+
+            double wallHeight = Double.valueOf(myScanner.nextLine());
+
+            System.out.println("What is the width of the wall?" );
+            double wallWidth = Double.valueOf(myScanner.nextLine());
+
+            double wallArea = wallHeight * wallWidth;
+            totalWallArea += wallArea;
+
+        }
+
+        return totalWallArea;
+    }
     public static void main(String[] args){
 
         Scanner myScanner = new Scanner(System.in);
@@ -9,39 +32,19 @@ public class PaintCalculator {
         System.out.println("How many walls are you painting?");
 
         wallNumber = Integer.valueOf(myScanner.nextLine());
-        double totalWallArea = 0;
 
-        for( int i = 1;  i <= wallNumber; i++ )
-        {
-            System.out.println("For wall number:  " + wallNumber + ". What are the dimensions?" );
-            System.out.println("What is the height of the wall?" );
+        double totalWallArea = totalWallArea(wallNumber);
 
-            double wallHeight = Double.valueOf(myScanner.nextLine());
+        int excludeWallNumber = 0;
+        System.out.println("How many areas on the wall need to be removed?");
+        excludeWallNumber = Integer.valueOf(myScanner.nextLine());
 
-            System.out.println("What is the width of the wall?" );
-            double wallWidth = Double.valueOf(myScanner.nextLine());
+        double excludeWallArea = totalWallArea(excludeWallNumber);
 
 
+        System.out.println(totalWallArea);
+        System.out.println();
 
-
-
-
-        }
-
-
-
-
-        System.out.println("What are the wall dimensions that you want to cover?");
-        System.out.println("Enter the Height in meters: ");
-
-        int x = 50;
-        int y = 50;
-
-        if (x == y){
-            System.out.println("Hey how are you?");
-        } else if (x>5) {
-
-        }
 
 
     }
